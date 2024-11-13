b11b93166ce6afd566748419fe5db3fddb1850a6
Kaze Wong <kazewong.physics@gmail.com>
2024-11-13 18:16:27 -0500

    Add placeholders for gradient descent animation steps in gradient_decent.py

--- project/gradient_decent.py
+++ project/gradient_decent.py
@@ -13,11 +13,17 @@ def target_function(x):
 
 class GradientDecent(Scene):
     def construct(self):
-        x = ValueTracker(0)
-        x.add_updater(lambda m, dt: m.increment_value(-STEP_SIZE * jax.grad(target_function)(m.get_value())))
-        graph = self.get_graph(target_function, color=BLUE)
-        dot = Dot().move_to(graph.points[0])
-        self.play(Create(graph), Create(dot), run_time=0.1)
-        self.play(x.set_value, 3, run_time=MAX_ITER * SPEEDUP_STEP)
-        self.wait()
 
+        # Plot the function
+
+        # Initialize the starting point
+
+        # Animate the gradient
+
+        # Update the point
+
+        # Repeat the process but speed up
+
+
+        # delete the following line
+        raise NotImplementedError("Please implement the code for plotting the function")
